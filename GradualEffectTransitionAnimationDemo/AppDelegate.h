1af1c4fc6c6b5ac7f676e48a897f94d571909d0a
wang66 <18693133051@163.com>
2017-04-10 09:16:05 +0800

    Initial Commit

---
+++ GradualEffectTransitionAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GradualEffectTransitionAnimationDemo
+//
+//  Created by 王亚文 on 2017/4/10.
+//  Copyright © 2017年 wyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
